9dca5676bbd2f857efb25bc202cc7c235790bafd
Dy4nk <aliermarron32@gmail.com>
2023-07-14 13:49:51 +0200

    Day 1 Finished

---
+++ 01/00_hello.py
@@ -0,0 +1,12 @@
+"""
+A plain,
+basic
+and boring Hello world.
+"""
+print('Hello, world!\n~ Dy4nk') 
+# print("Hello, world!\n~ Dy4nk")
+
+'''
+Not much to see here
+Not gonna lie
+'''
---
+++ 01/01_variables.py
@@ -0,0 +1,19 @@
+"""
+Some variables
+Not that interesting
+"""
+
+my_name = 'Chika chika Slim Shady'
+print(my_name)
+
+my_int = 313666744
+print(my_int)
+
+my_int_to_str = str(my_int)
+print(my_int_to_str)
+print(type(my_int_to_str))
+
+my_bool = False
+print(my_bool)
+
+print(my_name, my_int, my_bool)
